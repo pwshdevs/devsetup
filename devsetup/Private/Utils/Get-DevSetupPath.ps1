5d806261f58f694ecddd623ecdbfd34ebc5c9155
Joshua Wilson <kormic@gmail.com>
2025-08-29 01:09:07 -0500

    Trying again?

--- devsetup/Private/Utils/Get-DevSetupPath.ps1
+++
@@ -1,9 +0,0 @@
-Function Get-DevSetupPath {
-        # Get user's home directory
-        $homeDirectory = Get-EnvironmentVariable USERPROFILE
-        
-        # Define .devsetup folder path
-        $devSetupPath = Join-Path -Path $homeDirectory -ChildPath "devsetup"
-
-        return $devSetupPath
-}
